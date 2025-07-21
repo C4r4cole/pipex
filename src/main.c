e27493e0869e427c82fe289cf6a1e9f720dbee35
C4r4cole <c4r4cole.chevalerie@gmail.com>
2025-07-21 15:58:30 +0200

    main recovered

---
+++ src/main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 13:58:13 by fmoulin           #+#    #+#             */
+/*   Updated: 2025/07/21 15:57:15 by fmoulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	**args;
+
+	if (argc != 5)
+	{
+		write(2, "Error\n", 6);
+		return (EXIT_FAILURE);
+	}
+	else
+	{
+		args = argv + 1;
+		create_pipe(args, envp);
+	}
+	return (0);
+}
